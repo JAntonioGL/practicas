49effb0e3af35a13018be766035e64747146cffd
UrielEspinoza7 <espinoza.cebrero.uriel.13@gmail.com>
2026-09-08 16:11:03 -0600

    se guardó correctamente todo los cambios para logic

---
+++ calculadora_p_1/lib/logic/calculadora_core.dart
@@ -0,0 +1,27 @@
+//Aqui va toda la lógica de la calculadora
+
+/* Nota:
+int: Para números enteros (ej. 1, 50, -5).
+double: Para números con decimales (ej. 3.14, 0.5).
+num: Es un tipo general que puede aceptar tanto enteros (int) como decimales (double).
+*/
+
+//suma
+double suma(double num1, double num2) {
+  return num1 + num2;
+}
+
+//resta
+double resta(double num1, double num2) {
+  return num1 - num2;
+}
+
+//multiplicación
+double multiplicacion(double num1, double num2) {
+  return num1 * num2;
+}
+
+//division
+double division(double num1, double num2) {
+  return num1 / num2;
+}
