ecca6c8eab2b73aa8b473b29aef8b8920a31ee6f
UrielEspinoza7 <espinoza.cebrero.uriel.13@gmail.com>
2026-09-09 15:55:19 -0600

    feat: se agregó método _limpiar y se corrigieron erroes en variables de estado

--- calculadora_p_1/lib/screens/calculadora_screen.dart
+++ calculadora_p_1/lib/screens/calculadora_screen.dart
@@ -13,15 +13,28 @@ class CalculadoraScreen extends StatefulWidget {
 class _CalculadoraScreenState extends State<CalculadoraScreen> {
   //en esta clase se encuentra el estado de la calculadora, es decir, los datos que se van a mostrar en la pantalla
   // en este espacio se declaran varaibles que cambian
+  String _pantalla = '0'; //lo que s emuestra en pantalla y lleva un _ porque estamos diciendo que es pivada y solo la puede leer y modificar el mismo archivo que la creo
+  double? _primerNumero; // // guarda el primer calor y el signo ? nos dice que puede ser un valor nulo
+  String _operador =
+      ''; // guarda  el operador aritmetico que quiere usar '+', '-', '*', '/'
+  bool _esperandoNuevoNumero =
+      false; // Indica si el siguiente toque limpia la patalla o no
+
+  //------------------------------------------------------------------------
+  //haremos una función limpiar, como no es una función matematica no se coloca en calculadora_core.dart, sino aqui mismo en la clase
+  //ya que aqui es donde tenemos acceso a las variables de estado que necesitamos para limpiar pantalla como _pantalla
+  void _limpiar() {
+    setState(() {
+      _pantalla = '0';
+      _primerNumero = null;
+      _operador = '';
+      _esperandoNuevoNumero = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //aqui defines ques e dibuja en la pantalla
-    String _pantalla = '0'; //lo que s emuestra en pantalla y lleva un _ porque estamos diciendo que es pivada y solo la puede leer y modificar el mismo archivo que la creo
-    double? _primerNumero; // // guarda el primer calor y el signo ? nos dice que puede ser un valor nulo
-    String _operador =
-        ''; // guarda  el operador aritmetico que quiere usar '+', '-', '*', '/'
-    bool _esperandoNuevoNumero =
-        false; // Indica si el siguiente toque limpia la patalla o no
 
     return Scaffold(
       // se usa scaffold o andamio como estructura base del widget porque estamos usando material design
@@ -53,6 +66,20 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
             ),
           ),
           const Divider(color: Colors.blueGrey, height: 1),
+
+          //Aqui colocaremos las filas o rows de botones que conforman el teclado de la calculadora
+          //Fila 1. empezamos con al primer fila de botones limpiar y dividir
+          Row(
+            children: [
+              _construirBoton(
+                texto: 'C',
+                colorFondo: Colors.red[700],
+                alPresionar: () {
+                  //aqui llamamos a la función de operar
+                },
+              ),
+            ],
+          ), //termina la primer fila
         ],
       ), //SafeArea es un widget que se asegura de que el contenido no se superponga con el hardware de la pantalla, como el notch o la muesca de iPhone o la barra de navegación en Android. Center es un widget que centra su contenido en el centro de la pantalla. Text es un widget que muestra texto
     ); //Scaffold es el widget esqueleto que proporciona una estructura basica para la pantalla y se usa para el diseño visual de material design
@@ -76,14 +103,17 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
       child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: ElevatedButton(
+          //ElevatedButton es un widget de boton con relieve y sombra
           style: ElevatedButton.styleFrom(
-            backgroundColor: colorFondo ?? Colors.grey[850],
+            //definimos el estilo de este boton con ayuda de los parametros nombrados que soliciutamos
+            backgroundColor: colorFondo ?? Colors.grey[850], //los signos ?? se les llama Operador coalescente nulo, significa que si colorFondo no tiene un valor asignado osea null , se le asignara el valor de Colors.grey[850](generalmente si es nulo se hace la instrucción que está a la derecha del signo ??)
             padding: const EdgeInsets.symmetric(vertical: 22),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
           ),
-          onPressed: alPresionar,
+          onPressed: alPresionar, // onPressed es un parametro nombrado es una propiedad obligatoria  o callback que define la acción que se ejecuta cuando se presiona el boton
+          //tiene 2 estados , activo y desactivado (cuando está desactivado se ve de un color gris y no se puede presionar)
           child: Text(
             texto,
             style: TextStyle(
@@ -95,5 +125,5 @@ class _CalculadoraScreenState extends State<CalculadoraScreen> {
         ),
       ),
     ); // aqui termina el metodo _construirBoton
-  }
+  } //aqui termina el metodo _construirBoton
 }
