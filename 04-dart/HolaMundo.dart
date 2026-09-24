b907a833e1e49bdc679f67a9cee043251a937533
José Antonio Godoy <jantonio.godoylopez@gmail.com>
2026-09-24 17:47:50 -0600

    Migracion completa a typescript

---
+++ 04-dart/HolaMundo.dart
@@ -0,0 +1,28 @@
+void main() {
+  String nombre;
+  int edad_1;
+  double _precio = 55.20;
+  bool aCTivo = true;
+
+  final PI = 3.1416;
+  const valorConstante = 600;
+
+
+  var codigo = false;
+  
+
+  nombre = 'jose';
+  edad_1 = 65;
+
+
+
+  print(nombre);
+  print(edad_1);
+  print(_precio);
+  print(aCTivo);
+  print(codigo);
+
+  print(PI);
+  print(valorConstante);
+
+}
